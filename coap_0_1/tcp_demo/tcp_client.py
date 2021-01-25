c91275b04e50e36388e9db8f1d410631d18efb98
you-know-who-2017 <1376397133@qq.com>
2021-01-25 10:58:55 +0800

    update tcp

---
+++ coap_0_1/tcp_demo/tcp_client.py
@@ -0,0 +1,35 @@
+'''
+Author: geekli
+Date: 2021-01-25 10:58:26
+LastEditTime: 2021-01-25 10:58:46
+LastEditors: your name
+Description: 
+FilePath: /coap_0_1/tcp_demo/tcp_client.py
+'''
+import asyncio
+# replace ip address here
+raspberry_ip_addrss = '192.168.0.8'
+class EchoClientProtocol(asyncio.Protocol):
+    def __init__(self, message, loop):
+        self.message = message
+        self.loop = loop
+
+    def connection_made(self, transport):
+        transport.write(self.message.encode())
+        print('Data sent: {!r}'.format(self.message))
+
+    def data_received(self, data):
+        print('Data received: {!r}'.format(data.decode()))
+
+    def connection_lost(self, exc):
+        print('The server closed the connection')
+        print('Stop the event loop')
+        self.loop.stop()
+
+loop = asyncio.get_event_loop()
+message = 'Hello World!'
+coro = loop.create_connection(lambda: EchoClientProtocol(message, loop),
+                              raspberry_ip_addrss, 8090)
+loop.run_until_complete(coro)
+loop.run_forever()
+loop.close()
